6aef0942d49021e057c9a2fea36ce5a62b2bd127
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2023-06-11 20:25:56 +0530

    Hello world

---
+++ car.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+'''
+Evaluate The Car Policy
+'''
+
+import streamlit as st
+
+
+def main():
+    'The main function'
+
+    st.write("Hello")
+
+
+if __name__ == '__main__':
+    main()
