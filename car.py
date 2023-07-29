9aab63960500e927e389a6f5a736a80568c5c48f
Krishna <kasula@synopsys.com>
2023-07-29 08:23:47 +0530

    Movw

--- car.py
+++ car.py
@@ -16,8 +16,7 @@ class Input:
     'For taking input about the car Price etc.'
 
     cost: int = 1000000
-    fuel_and_maintenance_per_year: int = 270000
-    car_perquisite_per_month: int = 1800
+    engine_capacity: int = 1495
     tax_slab: float = 35.88
     actual_fuel_expense_percentage: float = 50.0
     duration_in_years: int = 5
@@ -28,12 +27,26 @@ class Input:
         return self.cost / 50
 
 
+    def fuel_and_maintenance_per_year(self):
+        if self.engine_capacity <= 1600:
+            return 270000
+
+        return 360000
+
+
+    def car_perquisite_per_month(self):
+        if self.engine_capacity <= 1600:
+            return 1800
+
+        return 2400
+
+
     def gross_per_month(self):
         return (
             self.cost / self.duration_in_years / 12
-            + self.fuel_and_maintenance_per_year / 12
+            + self.fuel_and_maintenance_per_year() / 12
             + self.insurance_per_year() / 12
-            + self.car_perquisite_per_month
+            + self.car_perquisite_per_month()
         )
 
 
@@ -69,7 +82,7 @@ class Input:
     def actual_fuel_expenses_per_month(self):
         'Return the actual fuel expenses'
 
-        return self.fuel_and_maintenance_per_year * self.actual_fuel_expense_percentage / 100 / 12
+        return self.fuel_and_maintenance_per_year() * self.actual_fuel_expense_percentage / 100 / 12
 
 
 def main():
@@ -77,7 +90,7 @@ def main():
 
     st.set_page_config(layout="wide")
 
-    locale.setlocale(locale.LC_MONETARY, 'en_IN.UTF-8')
+    locale.setlocale(locale.LC_MONETARY, 'en_IN')
 
     with st.sidebar:
         data = sp.pydantic_form(key="Input", model=Input)
@@ -85,9 +98,12 @@ def main():
     if not data:
         data = Input()
 
-    st.write(f'Gross monthly income: {locale.currency(data.gross_per_month(), grouping=True)}')
-    st.write(f'Actual Fuel Expenses/Month: {locale.currency(data.actual_fuel_expenses_per_month(), grouping=True)}')
-    st.json(json.dumps(data, default=pydantic_encoder))
+    output = {
+        'Gross monthly income': data.gross_per_month(),
+        'Actual Fuel Expenses/Month': 0 - data.actual_fuel_expenses_per_month()
+    }
+
+    st.dataframe({k: locale.currency(v, grouping=True) for k, v in output.items()})
 
 
 if __name__ == '__main__':
