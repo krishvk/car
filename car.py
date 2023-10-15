62ad8dd656afb81656dca6982b1c6fdff1a3e7e1
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2023-10-15 13:09:41 +0530

    All except the final comparision plot

--- car.py
+++ car.py
@@ -4,9 +4,10 @@ Evaluate The Car Policy
 '''
 import locale
 
-import stringcase
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+import stringcase
 
 
 class Car():
@@ -17,14 +18,12 @@ class Car():
         cost: int,
         above1600cc: bool,
         taxSlab: float,
-        actualDepreciation: float,
-        investmentReturns: float
+        actualDepreciation: float
     ):
         self.cost = cost
         self.above1600cc = above1600cc
         self.taxSlab = taxSlab
         self.actualDepreciation = actualDepreciation
-        self.investmentReturns = investmentReturns
 
 
     def getDurationInMonths(self):
@@ -81,17 +80,6 @@ class Car():
         return 1800
 
 
-    def getMinGrossSalPerMonth(self):
-        'Returns the minimum necessary gross salary per month to afford the car '
-
-        return (
-            self.cost / self.getDurationInMonths()
-            + self.fuelAndMaintenancePerMonth()
-            + self.insuranceEstimatePerYear() / 12
-            + self.carPerquisitePerMonth() * self.taxSlab / 100
-        )
-
-
     def depreciatedValue(self, originalValue: int, termInYears: int, depreciation: float):
         'Returns the depreciated value of an asset'
 
@@ -134,19 +122,62 @@ class Car():
             'cost': self.cost,
             'above1600cc': self.above1600cc,
             'taxSlab': self.taxSlab,
-            'actualDepreciation': self.actualDepreciation,
-            'investmentReturns': self.investmentReturns,
+            'actualDepreciation': self.actualDepreciation
         }
+        data['durationInYears'] = self.getDurationInYears()
         data['durationInMonths'] = self.getDurationInMonths()
         data['fuelAndMaintenancePerYear'] = self.fuelAndMaintenancePerYear()
         data['fuelAndMaintenancePerMonth'] = self.fuelAndMaintenancePerMonth()
         data['carPerquisitePerMonth'] = self.carPerquisitePerMonth()
         data['insuranceEstimatePerYear'] = self.insuranceEstimatePerYear()
         data['depreciationForTax'] = self.getDepreciationForTax()
-        data['minGrossSalPerMonth'] = self.getMinGrossSalPerMonth()
+        data['minGrossSalPerMonth'] = getMinGrossSalPerMonth(
+            self.cost,
+            self.getDurationInMonths(),
+            self.fuelAndMaintenancePerMonth(),
+            self.insuranceEstimatePerYear(),
+            self.carPerquisitePerMonth(),
+            self.taxSlab
+        )
         data['finalSellableValue'] = self.finalSellableValue()
 
         return data
+
+
+def getMinGrossSalPerMonth(
+    cost,
+    durationInMonths,
+    fuelAndMaintenancePerMonth,
+    insuranceEstimatePerYear,
+    carPerquisitePerMonth,
+    taxSlab
+):
+    'Returns the minimum necessary gross salary per month to afford the car '
+
+    return (
+        cost / durationInMonths
+        + fuelAndMaintenancePerMonth
+        + insuranceEstimatePerYear / 12
+        + carPerquisitePerMonth * taxSlab / 100
+    )
+
+
+def calcDepreciatedValue(originalValue, termInYears, depreciation):
+    'Calculate the depreciated value of an asset'
+
+    return originalValue * ((1 - depreciation / 100) ** termInYears)
+
+
+def calcFinalTaxOnTransfer(cost, durationInYears, durationInMonths, taxSlab, depreciationForTax):
+    'Calculate the final tax on transfer'
+
+    return (
+        # Depreciation is calculated 1 month before the end of the term
+        # Thus effectively 1 year less depreciation
+        calcDepreciatedValue(cost, durationInYears - 1, depreciationForTax)
+        # For last EMI shown as purchase value
+        - (cost / durationInMonths)
+    ) * taxSlab / 100
 
 
 def showDictInTable(data, currency=False, percentage=False):
@@ -198,16 +229,16 @@ def styleCurrency(v):
     return locale.currency(v, grouping=True)
 
 
-def styleRed(v):
+def styleRed(v): # pylint: disable=unused-argument
     'Style the value in red'
 
-    return f'color:red;'
+    return 'color:red;'
 
 
-def styleGreen(v):
+def styleGreen(v): # pylint: disable=unused-argument
     'Style the value in green'
 
-    return f'color:green;'
+    return 'color:green;'
 
 
 def styleNegative(v):
@@ -245,37 +276,30 @@ def showFistCarData(data):
         .map(styleRed, subset=['Perq Tax', 'Tax on Transfer']) \
         .map(styleGreen, subset=['Tax Savings (EFMI)', 'Seperation Cost'])
 
-    col1, col2 = st.columns([2, 1])
+    col1, col2 = st.columns([3, 2])
     with col1:
         # Show the final data
-        st.dataframe(styledDf, use_container_width=True, height=500)
+        st.dataframe(styledDf, use_container_width=True, height=450)
     with col2:
-        st.line_chart(df[['Tax on Transfer', 'Seperation Cost']])
+        st.plotly_chart(
+            px.line(df, y=['Tax on Transfer', 'Seperation Cost']).update_layout(
+                xaxis_title='Months',
+                yaxis_title='Amount',
+            ),
+            use_container_width=True
+        )
+
+    st.write(f'''
+        At the end of tenure, you would save
+        * {styleCurrency(df.iloc[-1]['Seperation Cost'])} in taxes over the tenure and
+        * Posses a car with a sellable value of {styleCurrency(data['finalSellableValue'])}
+        * With a total gain (unadjusted over time) of
+        {styleCurrency(df.iloc[-1]['Seperation Cost'] + data['finalSellableValue'])}
+    ''')
 
 
-def showSecondCarData(data):
-    'Show the data for the second car'
-
-    df = pd.DataFrame.from_records(
-        {
-            'Old Car': {
-                'Gross Post Tax': data['minGrossSalPerMonth'] * (1 - data['taxSlab'] / 100),
-                'EMI': 0,
-                'Fuel & Maintenance': 0 - data['fuelAndMaintenancePerMonth'],
-                'Insurance': 0 - data['insuranceEstimatePerYear'] / 12,
-                'Tax on Car Perq': 0
-            },
-            'New Car': {
-                'Gross Post Tax': data['minGrossSalPerMonth'],
-                'EMI': 0 - data['cost'] / data['durationInMonths'],
-                'Fuel & Maintenance': 0 - data['fuelAndMaintenancePerMonth'],
-                'Insurance': 0 - data['insuranceEstimatePerYear'] / 12,
-                'Tax on Car Perq': 0 - data['carPerquisitePerMonth'] * data['taxSlab'] / 100
-            }
-        }
-    )
-
-    df.loc["Total"] = df.sum()
+def calcSipFinalValue(sipAmount, durationInMonths, investmentReturns):
+    'Calculate the final value of SIP'
 
     # FV = Future value or the amount you get at maturity.
     # FV = P [ (1+i)^n-1 ] * (1+i)/i
@@ -284,19 +308,146 @@ def showSecondCarData(data):
     # n = Investment duration in months
     # r = Expected rate of return
 
-    i = data['investmentReturns'] / 1200
-    df.loc['Projected Final Value'] = df.loc['Total'] * (
-        (1 + i) ** data['durationInMonths'] - 1
-    ) * (1 + i) / i
+    i = investmentReturns / 1200
+    return sipAmount * ((1 + i) ** durationInMonths - 1) * (1 + i) / i
 
-    df['Old Car'].loc['Result']  = 0
-    df['New Car'].loc['Result']  = 0
 
-    styledDf = df.style \
-        .format(lambda i: locale.currency(i, grouping=True)) \
-        .map(styleNegative)
+def calcLumpsumFinalValue(lumpsumAmount, durationInMonths, investmentReturns):
+    'Calculate the final value of lumpsum investment'
 
-    st.dataframe(styledDf, use_container_width=True, column_order=['Old Car', 'New Car'])
+    # FV = Future value or the amount you get at maturity.
+    # FV = P * (1+i)^n
+    # P = Amount you invest through SIP
+    # i = Compounded rate of return
+    # n = Investment duration in months
+    # r = Expected rate of return
+
+    i = investmentReturns / 1200
+    return lumpsumAmount * ((1 + i) ** durationInMonths)
+
+
+def showSecondCarData(data):
+    'Show the data for the second car'
+
+    def showMonthlyData(data):
+        'Show the monthly data'
+
+        # st.header('')
+        st.write('Monthly Paycheck (During the tenure)')
+        df = pd.DataFrame.from_records(
+            {
+                'Old Car': {
+                    'Gross Post Tax': data['minGrossSalPerMonth'] * (1 - data['taxSlab'] / 100),
+                    'Fuel & Maintenance': 0 - data['fuelAndMaintenancePerMonth'],
+                    'Insurance': 0 - data['insuranceEstimatePerYear'] / 12,
+                },
+                'New Car': {
+                    'Gross Post Tax': data['minGrossSalPerMonth'],
+                    'EMI': 0 - data['cost'] / data[ 'durationInMonths'],
+                    'Fuel & Maintenance': 0 - data['fuelAndMaintenancePerMonth'],
+                    'Insurance': 0 - data['insuranceEstimatePerYear'] / 12,
+                    'Tax on Car Perq': 0 - data['carPerquisitePerMonth'] * data['taxSlab'] / 100
+                }
+            }
+        )
+
+        df.loc["Investable Balance"] = df.sum().round(2).apply(lambda i: i if i != 0 else None)
+
+        st.dataframe(
+            df.style.format(lambda i: locale.currency(i, grouping=True)).map(styleNegative),
+            use_container_width=True,
+            column_order=['Old Car', 'New Car']
+        )
+
+        return df
+
+
+    def showFinalProjections(data, monthlyDf, saleValueOfOldCar, investmentReturns):
+        'Show the final projections'
+
+        # st.header('Projected Final Values')
+        st.write('Projected Final Values (At the end of the tenure)')
+
+        df = pd.DataFrame.from_records(
+            {
+                'Old Car': {
+                    'Investable Balance': calcSipFinalValue(
+                        monthlyDf.loc['Investable Balance', 'Old Car'],
+                        data['durationInMonths'],
+                        investmentReturns
+                    ),
+                    'Sellable value': calcDepreciatedValue(
+                        saleValueOfOldCar,
+                        data['durationInYears'],
+                        data['actualDepreciation']
+                    ),
+                },
+                'New Car': {
+                    'Sale Proceedings from Old Car': calcLumpsumFinalValue(
+                        saleValueOfOldCar,
+                        data['durationInMonths'],
+                        investmentReturns
+                    ),
+                    'Tax on Transfer': 0 - calcFinalTaxOnTransfer(
+                        data['cost'],
+                        data['durationInYears'],
+                        data['durationInMonths'],
+                        data['taxSlab'],
+                        data['depreciationForTax']
+                    ),
+                    'Sellable value': calcDepreciatedValue(
+                        data['cost'],
+                        data['durationInYears'],
+                        data['actualDepreciation']
+                    )
+                }
+            }
+        )
+
+        df.loc["Total"] = df.sum().round(2)
+
+        st.dataframe(
+            df.style.format(lambda i: locale.currency(i, grouping=True)).map(styleNegative),
+            use_container_width=True,
+            column_order=['Old Car', 'New Car']
+        )
+
+
+    def plotCostComparision(data):
+        'Plot the comparision between the 2 options for different values of new car'
+
+        df = pd.DataFrame(index=range(1000000, 9000000, 100000))
+        df['Old Car'] = df.index
+        df['New Car'] = df.index + 2
+
+        st.plotly_chart(
+            px.line(df, y=['Old Car', 'New Car']).update_layout(
+                xaxis_title='Cost of New Car',
+                yaxis_title='Net Gain at the end of tenure',
+            ),
+            use_container_width=True
+        )
+
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        saleValueOfOldCar = st.number_input(
+            'Sale Value of Old Car',
+            value=1000000,
+            step=10,
+            format='%d'
+        )
+    with col2:
+        investmentReturns = st.slider(
+            'ROI(%)', value=20, max_value=100, format='%f',
+            help='Expected investment returns on the amount saved'
+        )
+
+    table, chart = st.columns([1, 2])
+    with table:
+        df = showMonthlyData(data)
+        showFinalProjections(data, df, saleValueOfOldCar, investmentReturns)
+    with chart:
+        plotCostComparision(data)
 
 
 def getTaxSlabs():
@@ -325,15 +476,11 @@ def getInputs():
         help='Including Surcharge and Cess'
     )
     actualDepreciation = st.slider(
-        'Depreciation(%)', value=10, max_value=100, format='%f',
+        'Actual Depreciation(%)', value=10, max_value=100, format='%f',
         help='Yearly depreciation expected for resale'
     )
-    investmentReturns = st.slider(
-        'ROI(%)', value=20, max_value=100, format='%f',
-        help='Expected investment returns on the amount saved'
-    )
 
-    return Car(cost, above1600cc == 'Yes', taxSlab, actualDepreciation, investmentReturns)
+    return Car(cost, above1600cc == 'Yes', taxSlab, actualDepreciation)
 
 
 def main():
@@ -346,6 +493,7 @@ def main():
     # dataCol, analysisCol = st.columns([1, 5])
     with st.sidebar:
         data = getInputs().getDict()
+        st.divider()
         showData(data)
 
     # with analysisCol:
