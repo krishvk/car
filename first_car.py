17ae73f1bed3777f05782f0a8e31ca1323594f13
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2024-02-11 09:25:53 +0530

    Add SIP component

--- first_car.py
+++ first_car.py
@@ -13,6 +13,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+import finance_utils as fu
 import streamlit_utils as su
 
 
@@ -29,37 +30,42 @@ def show(data):
         + df['Paid']
     ) * data['taxSlab'] / 100
     df['Perq Tax'] = data['carPerquisitePerMonth'] * df.index * data['taxSlab'] / 100
+    df['Investable Savings'] = df['Tax Savings (EFMI)'] - df['Perq Tax']
+    print(data['investmentReturns'])
+    df['Savings Invested'] = df.apply(
+        lambda i: fu.calcSipFinalValue(
+            df['Investable Savings'][1],
+            i.name,
+            data['investmentReturns']
+        ),
+        axis=1
+    )
     df['Depreciated Value'] = data['cost'] * (
         (1 - data['depreciationForTax'] / 100) **
         (df.index / 12).astype(int)
     )
     df['Tax on Transfer'] = (df['Depreciated Value'] - df['Remaining']) * data['taxSlab'] / 100
-    df['Seperation Cost'] = df['Tax Savings (EFMI)'] - df['Perq Tax'] - df['Tax on Transfer']
+    df['Seperation Cost'] = df['Savings Invested'] - df['Tax on Transfer']
 
     # Format the numbers in the output
     styledDf = df.style \
         .format(lambda i: locale.currency(i, grouping=True)) \
         .map(su.styleRed, subset=['Perq Tax', 'Tax on Transfer']) \
-        .map(su.styleGreen, subset=['Tax Savings (EFMI)', 'Seperation Cost'])
-
-    col1, col2 = st.columns([3, 2])
-    with col1:
-        # Show the final data
-        st.dataframe(styledDf, use_container_width=True, height=450)
-    with col2:
-        st.plotly_chart(
-            px.line(df, y=['Tax on Transfer', 'Seperation Cost']).update_layout(
-                xaxis_title='Months',
-                yaxis_title='Amount',
-            ),
-            use_container_width=True
+        .map(
+            su.styleGreen,
+            subset=[
+                'Tax Savings (EFMI)', 'Seperation Cost', 'Investable Savings', 'Savings Invested'
+            ]
         )
 
+    st.dataframe(styledDf, use_container_width=True, height=450)
+
     st.write(f'''
-        At the end of tenure, you would save
-        * {su.styleCurrency(df.iloc[-1]['Seperation Cost'])} in taxes over the tenure and
+        At the end of tenure, you would have
+        * {su.styleCurrency(df.iloc[-1]['Seperation Cost'])} in invested savings
+        * After paying a transfer tax of {su.styleCurrency(df.iloc[-1]['Tax on Transfer'])} and
         * Posses a car with a sellable value of {su.styleCurrency(data['finalSellableValue'])}
-        * With a total gain (unadjusted over time) of
+        * With a total gain (adjusted over time) of
         {su.styleCurrency(df.iloc[-1]['Seperation Cost'] + data['finalSellableValue'])}
     ''')
 
